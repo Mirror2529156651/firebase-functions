145092239e8b53726393326b2f94329b7862d423
Mirror <jess2529156651@gmail.com>
2020-07-14 16:51:21 +0800

    test

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+module.exports = {
+    get_data: functions.https.onRequest((req, res) => {
+        res.send(req.url);
+    })
+}
